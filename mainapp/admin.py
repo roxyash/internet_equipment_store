74b09746613cf1c3868c6c0a29f1e7846eba1882
roxyash <47809943+roxyash@users.noreply.github.com>
2021-02-01 22:42:40 +0300

    Bag AnonymousUser need Fix

---
+++ mainapp/admin.py
@@ -0,0 +1,59 @@
+from django.contrib import admin
+from django.forms import ModelChoiceField, ModelForm, ValidationError
+from django.utils.safestring import mark_safe
+from PIL import Image
+
+from .models import *
+
+
+# class NotebookAdminForm(ModelForm):
+#
+#     def __init__(self, *args, **kwargs):
+#         super().__init__(*args, **kwargs)
+#         self.fields['image'].help_text = mark_safe(
+#             """<span style="color:red;font-size:14px;">При загрузке изображения с разрешением больше {}x{} оно будет
+#             обрезано</span> """
+#                 .format(
+#                 *Product.MAX_RESOLUTION
+#             )
+#         )
+# Предупреждение пользователя, об загружаемом формате изображения и его размерах
+# def clean_image(self):
+#     image = self.cleaned_data['image']
+#     img = Image.open(image)
+#     min_height, min_width = Product.MIN_RESOLUTION
+#     max_height, max_width = Product.MAX_RESOLUTION
+#     if image.size > Product.MAX_IMAGE_SIZE:
+#         raise ValidationError('Размер изображение не должен привышать 3МБ')
+#     if img.height < min_height or img.width < min_width:
+#         raise ValidationError('Разрешение изображения, меньше минимального')
+#     if img.height > max_height or img.width > max_width:
+#         raise ValidationError('Разрешение изображения, больше максимального')
+#     super().save(*args, **kwargs)
+
+
+class NotebookAdmin(admin.ModelAdmin):
+    # form = NotebookAdminForm
+
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        if db_field.name == 'category':
+            return ModelChoiceField(Category.objects.filter(slug='notebooks'))
+        return super().formfield_for_foreignkey(db_field, request, **kwargs)
+
+
+class SmartphoneAdmin(admin.ModelAdmin):
+
+    change_form_template = 'admin.html'
+
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        if db_field.name == 'category':
+            return ModelChoiceField(Category.objects.filter(slug='smartphones'))
+        return super().formfield_for_foreignkey(db_field, request, **kwargs)
+
+
+admin.site.register(Category)
+admin.site.register(Notebook, NotebookAdmin)
+admin.site.register(Smartphone, SmartphoneAdmin)
+admin.site.register(CartProduct)
+admin.site.register(Cart)
+admin.site.register(Customer)
---
+++ mainapp/views.py
@@ -0,0 +1,86 @@
+from .models import Notebook, Smartphone
+from django.http import HttpResponseRedirect
+from django.contrib.contenttypes.models import ContentType
+from django.shortcuts import render
+from django.views.generic import DetailView, View
+from .models import Notebook, Smartphone, Category, LatestProducts, Customer, Cart, CartProduct
+from .mixins import CategoryDetailMixin
+
+
+class BaseView(View):
+
+    def get(self,request,*args, **kwargs):
+        categories = Category.objects.get_categories_for_left_sidebar()
+        products = LatestProducts.objects.get_products_for_main_page(
+            'notebook', 'smartphone'
+        )
+        context = {
+            'categories': categories,
+            'products': products
+        }
+        return render(request, 'base.html', context)
+
+
+class ProductDetailView(CategoryDetailMixin, DetailView):
+
+    CT_MODEL_MODEL_CLASS = {
+        'notebook': Notebook,
+        'smartphone': Smartphone
+
+    }
+
+    def dispatch(self, request, *args, **kwargs):
+        self.model = self.CT_MODEL_MODEL_CLASS[kwargs['ct_model']]
+        self.queryset = self.model._base_manager.all()
+        return super().dispatch(request, *args, **kwargs)
+
+    context_object_name = 'product'
+    template_name = 'product_detail.html'
+    slug_url_kwarg = 'slug'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['ct_model'] = self.model._meta.model_name
+        return context
+
+
+class CategoryDetailView(CategoryDetailMixin, DetailView):
+
+    model = Category
+    queryset = Category.objects.all()
+    context_object_name = 'category'
+    template_name = 'category_detail.html'
+    slug_url_kwarg = 'slug'
+
+
+class AddToCartView(View):
+
+    def get(self, request, *args, **kwargs):
+        ct_model, product_slug = kwargs.get('ct_model'), kwargs.get('slug')
+        customer = Customer.objects.get(user=request.user)
+        cart = Cart.objects.get(owner=customer, in_order=False)
+        content_type = ContentType.objects.get(model=ct_model)
+        product = content_type.model_class().objects.get(slug=product_slug)
+        cart_product = CartProduct.objects.create(
+            user=cart.owner, cart=cart, content_object=product, final_price=product.price
+        )
+        cart.products.add(cart_product)
+        return HttpResponseRedirect('/cart/')
+
+
+class CartView(View):
+
+    def get(self, request, *args, **kwargs):
+        customer = Customer.objects.get(user=request.user)
+        try:
+            cart = Cart.objects.get(owner=customer)
+        except Cart.DoesNotExist:
+            cart = None
+
+        categories = Category.objects.get_categories_for_left_sidebar()
+        context = {
+            'cart': cart,
+            'categories': categories
+        }
+        return render(request, 'cart.html', context)
+
